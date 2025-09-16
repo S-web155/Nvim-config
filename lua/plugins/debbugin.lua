a54961787547fe5e4f846bd88ef1cc3925f5f1ca
Sanchit Jaswal <sanchitjaswal@gmail.com>
2025-09-16 15:15:18 +0530

    configured debbugger for python

--- lua/plugins/debbugin.lua
+++ lua/plugins/debbugin.lua
@@ -1,7 +1,33 @@
 return{
   'mfussenegger/nvim-dap',
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "rcarriga/nvim-dap-ui",
+    'mfussenegger/nvim-dap-python',
+
+  },
   config = function()
     local dap = require('dap')
-     vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
-  end
+    local dapui = require("dapui")
+
+    require("dap-python").setup("C:/Users/vinod/.virtualenvs/debugpy/Scripts/python")
+    require("dapui").setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+    vim.keymap.set('n', '<space>b', dap.toggle_breakpoint, {})
+    vim.keymap.set('n', '<space>c', dap.continue, {})
+  end,
 }
+
+
