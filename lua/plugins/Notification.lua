45a76cf8042211cfc7a167e3ea084dec451272c8
Sanchit Jaswal <sanchitjaswal@gmail.com>
2025-09-17 13:53:42 +0530

    Added noice for notification

--- lua/plugins/Notification.lua
+++ lua/plugins/Notification.lua
@@ -1,14 +1,38 @@
 return{
-  'rcarriga/nvim-notify',
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  },
   config = function()
-    require('notify').setup({
-      top_down = false,
-      timeout = 3000,
-      level = 3,
-      stages = 'slide',
-      opacity = 0.8
+    require("noice").setup({
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
     })
-    vim.notify = require('notify')
-    vim.keymap.set('n', '<space>n', ':Telescope notify<CR>', {})
-  end
+    require('notify').setup({
+      stages = 'slide'
+    })
+  end,
 }
